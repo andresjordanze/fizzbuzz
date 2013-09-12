473f3a6b7175e597b2b317725def5173836d5e2f
andresjordanze <andresjordanze@gmail.com>
2013-09-12 01:04:02 -0400

    numeros del 1 al 100

---
+++ src/main/java/numeros/numero.java
@@ -0,0 +1,12 @@
+package numeros;
+
+public class numero {
+	private int tam = 100;
+	
+	public void imprimir(){
+		for(int i=0;i<tam;i++){
+			System.out.println(i);
+		}
+	}
+}
+
---
+++ src/test/java/numeros/numeroTest.java
@@ -0,0 +1,13 @@
+package numeros;
+
+import org.junit.Test;
+
+public class numeroTest {
+
+	@Test
+	public void imprimirNumdel1_100() {
+		numero nums = new numero();
+		nums.imprimir();
+	}
+
+}
