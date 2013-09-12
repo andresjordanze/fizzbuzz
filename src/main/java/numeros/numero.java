4c71d6b606f14750656e633c9e6396570941c47c
andresjordanze <andresjordanze@gmail.com>
2013-09-12 01:11:34 -0400

    fizzMultiplos3

--- src/main/java/numeros/numero.java
+++ src/main/java/numeros/numero.java
@@ -4,8 +4,13 @@ public class numero {
 	private int tam = 100;
 	
 	public void imprimir(){
-		for(int i=0;i<tam;i++){
-			System.out.println(i);
+		for(int i=1;i<=tam;i++){
+			
+			if(i%3==0){
+				System.out.println("Fizz");
+			}else{
+				System.out.println(i);
+			}
 		}
 	}
 }
